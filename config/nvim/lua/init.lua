9a85a344a4d825c712098f1c7742348cfba0be29
Alex Repavich <alex.repavich@cybergrx.com>
2022-08-11 15:05:09 +0000

    Disabling exrc

--- config/nvim/lua/init.lua
+++ config/nvim/lua/init.lua
@@ -2,8 +2,6 @@ require('utils.reload')
 vim.cmd('filetype plugin indent on')
 vim.opt.termguicolors = true
 vim.opt.number = true
-vim.opt.exrc = true
-vim.opt.secure = true
 -- vim.opt.tabstop = 4
 -- vim.opt.shiftwidth = 0
 -- vim.opt.softtabstop = 0
