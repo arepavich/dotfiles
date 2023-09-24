e42beea2e0bf5b428da0fffdd46b32098818c0de
Alex Repavich <merry.iron1816@fastmail.com>
2023-09-24 17:14:19 -0500

    Update tab settings and scrolloff

--- config/nvim/lua/init.lua
+++ config/nvim/lua/init.lua
@@ -4,10 +4,11 @@ vim.opt.termguicolors = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.mouse = ""
--- vim.opt.tabstop = 4
--- vim.opt.shiftwidth = 0
--- vim.opt.softtabstop = 0
--- vim.opt.expandtab = true
+vim.opt.scrolloff = 8
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 0
+vim.opt.softtabstop = 0
+vim.opt.expandtab = true
 
 
 
